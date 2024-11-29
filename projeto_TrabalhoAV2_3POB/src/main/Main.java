416014e2a7ba92149257a0ed74d74e63eac06036
joao_Vitor_Marques_Xavier <100618696+Jvitormx@users.noreply.github.com>
2024-11-28 21:00:26 -0300

    TrabalhoAV2 - 3POB - 2024.2

---
+++ projeto_TrabalhoAV2_3POB/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+/*
+ * Joao Vitor Marques Xavier - 23104708360056
+ * 3POB - 2024.2
+ */
+import view.FuncionarioView;
+
+public class Main {
+    public static void main(String[] args) {
+        FuncionarioView view = new FuncionarioView();
+        view.menu();
+    }
+}
---
+++ projeto_TrabalhoAV2_3POB/src/model/Gerente.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Gerente extends Funcionario implements Gerencia {
+
+    public Gerente(int id, String nome, double salario) {
+        super(id, nome, salario);
+    }
+
+    @Override
+    public void organizarEquipe() {
+        System.out.println(getNome() + " está organizando a equipe.");
+    }
+
+    @Override
+    public void conduzirReunioes() {
+        System.out.println(getNome() + " está conduzindo reuniões.");
+    }
+
+    @Override
+    public String mostrarDetalhes() {
+        return "Gerente: " + getNome() + ", Salário: " + getSalario();
+    }
+}
